2ad910bea0a36c264b8eee29e113db97540a7d7a
LUISA FERNANDA VERGARA SEPULVEDA <fernandavergara0218@gmail.com>
2021-09-03 21:08:01 -0500

    Add Vehicle Type

---
+++ Vehicles.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Vehicles.API.Data.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
+        }
+    }
+}
