03854bdba64dc403da9c9090a9d5b6b5b7e5cf0d
LUISA FERNANDA VERGARA SEPULVEDA <fernandavergara0218@gmail.com>
2021-09-14 02:09:26 -0500

    Add Controller Users

---
+++ Vehicles.API/Helpers/CombosHelper.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicles.API.Helpers
+{
+    public class CombosHelper : ICombosHelper
+    {
+        private readonly DataContext _context;
+
+        public CombosHelper(DataContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<SelectListItem> GetCombosBrands()
+        {
+            List<SelectListItem> list = _context.Brands.Select(x => new SelectListItem
+            {
+                Text = x.Description,
+                Value = $"{x.Id}"
+            })
+                .OrderBy(x => x.Text)
+                .ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione una marca...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+        public IEnumerable<SelectListItem> GetCombosProcedures()
+        {
+            List<SelectListItem> list = _context.Procedures.Select(x => new SelectListItem
+            {
+                Text = x.Description,
+                Value = $"{x.Id}"
+            })
+                 .OrderBy(x => x.Text)
+                 .ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione un procedimiento...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+        public IEnumerable<SelectListItem> GetCombosDocumentsTypes()
+        {
+            List<SelectListItem> list = _context.DocumentTypes.Select(x => new SelectListItem
+            {
+                Text = x.Description,
+                Value = $"{x.Id}"
+            })
+                .OrderBy(x => x.Text)
+                .ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione un tipo de documento...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+
+
+        public IEnumerable<SelectListItem> GetCombosVehiclesTypes()
+        {
+            List<SelectListItem> list = _context.VehicleTypes.Select(x => new SelectListItem
+            {
+                Text = x.Description,
+                Value = $"{x.Id}"
+            })
+                .OrderBy(x => x.Text)
+                .ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "[Seleccione un tipo de vehículo...]",
+                Value = "0"
+            });
+
+            return list;
+        }
+    }
+
+ }
+
--- Vehicles.API/Startup.cs
+++ Vehicles.API/Startup.cs
@@ -45,6 +45,7 @@ namespace Vehicles.API
 
             services.AddTransient<SeedDb>();
             services.AddScoped<IUserHelper, UserHelper>();
+            services.AddScoped<ICombosHelper, CombosHelper>();
 
         }
 
