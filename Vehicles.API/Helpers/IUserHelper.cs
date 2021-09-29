c00adea3b0170b9310fb7ab8be84bee0f8078aee
LUISA FERNANDA VERGARA SEPULVEDA <fernandavergara0218@gmail.com>
2021-09-28 20:36:52 -0500

    Selft Registration user

--- Vehicles.API/Helpers/IUserHelper.cs
+++ Vehicles.API/Helpers/IUserHelper.cs
@@ -3,15 +3,18 @@ using System;
 using System.Threading.Tasks;
 using Vehicles.API.Data.Entities;
 using Vehicles.API.Models;
+using Vehicles.Common.Enuims;
 
 namespace Vehicles.API.Helpers
 {
     public interface IUserHelper
     {
-        Task<User> GetUserAsync(string email);
+       Task<User> GetUserAsync(string email);
        Task<User> GetUserAsync(Guid id);
 
        Task<IdentityResult> AddUserAsync(User user, string password);
+
+        Task<User> AddUserAsync(AddUserViewModel model, Guid imageId, UserType userType);
 
         Task<IdentityResult> UpdateUserAsync(User user);
 
