d04e2a49bcc6e424ef812538c9643a512cc50d1d
Luisa0218 <78153272+Luisa0218@users.noreply.github.com>
2021-10-29 05:51:58 -0500

    Brands Controller Api

--- Vehicles.API/Controllers/API/BrandsController.cs
+++ Vehicles.API/Controllers/API/BrandsController.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Vehicles.API.Data.Entities;
 
 namespace Vehicles.API.Controllers.API
@@ -14,47 +13,46 @@ namespace Vehicles.API.Controllers.API
     [ApiController]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
-    
-    public class ProceduresController : ControllerBase
+
+    public class BrandsController : ControllerBase
     {
         private readonly DataContext _context;
 
-        public ProceduresController(DataContext context)
+        public BrandsController(DataContext context)
         {
             _context = context;
         }
 
-       
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Procedure>>> GetProcedures()
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
         {
-            return await _context.Procedures.OrderBy(x =>x.Description).ToListAsync();
+            return await _context.Brands.OrderBy(x => x.Description).ToListAsync();
         }
 
-       
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Procedure>> GetProcedure(int id)
-        {
-            var procedure = await _context.Procedures.FindAsync(id);
 
-            if (procedure == null)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Brand>> GetBrand(int id)
+        {
+            Brand brand = await _context.Brands.FindAsync(id);
+
+            if (brand == null)
             {
                 return NotFound();
             }
 
-            return procedure;
+            return brand;
         }
 
-      
+
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutProcedure(int id, Procedure procedure)
+        public async Task<IActionResult> PutBrand(int id, Brand brand)
         {
-            if (id != procedure.Id)
+            if (id != brand.Id)
             {
                 return BadRequest();
             }
 
-            _context.Entry(procedure).State = EntityState.Modified;
+            _context.Entry(brand).State = EntityState.Modified;
 
             try
             {
@@ -65,36 +63,34 @@ namespace Vehicles.API.Controllers.API
             {
                 if (dbUpdateException.InnerException.Message.Contains("duplicate"))
                 {
-                    return BadRequest( "Ya existe este procedimiento.");
+                    return BadRequest("Ya existe esta marca.");
                 }
                 else
                 {
-                    return BadRequest( dbUpdateException.InnerException.Message);
+                    return BadRequest(dbUpdateException.InnerException.Message);
                 }
             }
             catch (Exception exception)
             {
-                return BadRequest( exception.Message);
+                return BadRequest(exception.Message);
             }
 
         }
 
-        
         [HttpPost]
-        public async Task<ActionResult<Procedure>> PostProcedure(Procedure procedure)
+        public async Task<ActionResult<Brand>> PostBrand(Brand brand)
         {
-            _context.Procedures.Add(procedure);
-
+            _context.Brands.Add(brand);
             try
             {
                 await _context.SaveChangesAsync();
-                return CreatedAtAction("GetProcedure", new { id = procedure.Id }, procedure);
+                return CreatedAtAction("GetBrand", new { id = brand.Id }, brand);
             }
             catch (DbUpdateException dbUpdateException)
             {
                 if (dbUpdateException.InnerException.Message.Contains("duplicate"))
                 {
-                    return BadRequest("Ya existe este tipo de vehículo.");
+                    return BadRequest("Ya existe esta marca.");
                 }
                 else
                 {
@@ -110,19 +106,20 @@ namespace Vehicles.API.Controllers.API
 
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteProcedure(int id)
+        public async Task<IActionResult> DeleteBrand(int id)
         {
-            var procedure = await _context.Procedures.FindAsync(id);
-            if (procedure == null)
+            Brand brand = await _context.Brands.FindAsync(id);
+            if (brand == null)
             {
                 return NotFound();
             }
 
-            _context.Procedures.Remove(procedure);
+            _context.Brands.Remove(brand);
             await _context.SaveChangesAsync();
 
             return NoContent();
         }
+
 
     }
 }
